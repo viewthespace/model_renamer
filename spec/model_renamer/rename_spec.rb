b0cae69918a038809c6087212fcbd0c8c40d3ae1
Karl Baum <karl.baum@gmail.com>
2017-10-05 11:46:53 -0400

    Add basic rename test

--- spec/model_renamer/rename_spec.rb
+++ spec/model_renamer/rename_spec.rb
@@ -1,3 +1,16 @@
 describe Rename do
-  pending "write it"
+  around do |example|
+    MemFs.activate { example.run }
+  end
+
+  describe 'rename Cat to Dog' do
+    before do
+      File.open('./tmp/cat.rb', 'w') { |f| f.puts "hello world" }
+      Rename.new("Cat", "Dog").rename
+    end
+
+    it 'renames the file' do
+      expect(File.exists?('./tmp/dog.rb')).to be true
+    end
+  end
 end
