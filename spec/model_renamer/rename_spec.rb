dfc08863b4aa1c0d27179afdf2009cc4864ccc16
Karl Baum <karl.baum@gmail.com>
2017-10-06 08:28:18 -0400

    Add controller test case

--- spec/model_renamer/rename_spec.rb
+++ spec/model_renamer/rename_spec.rb
@@ -4,31 +4,56 @@ describe Rename do
   end
 
   describe 'rename Cat to Dog' do
-    def cat_content
+    def cat_class_content
       <<~HEREDOC
         class Cat
         end
       HEREDOC
     end
 
-    def dog_content
+    def cat_controller_content
+      <<~HEREDOC
+        class CatsController < ApplicationController
+          def show
+            @cat = Cat.new
+          end
+        end
+      HEREDOC
+    end
+
+    def dog_class_content
       <<~HEREDOC
         class Dog
         end
       HEREDOC
     end
 
+    def dog_controller_content
+      <<~HEREDOC
+        class DogsController < ApplicationController
+          def show
+            @dog = Dog.new
+          end
+        end
+      HEREDOC
+    end
+
     before do
-      File.open('./tmp/cat.rb', 'w') { |f| f.write cat_content }
+      FileUtils.mkdir_p './app/models'
+      FileUtils.mkdir_p './app/controllers'
+      File.open('./app/models/cat.rb', 'w') { |f| f.write cat_class_content }
+      File.open('./app/controllers/cats_controller.rb', 'w') { |f| f.write cat_controller_content }
       Rename.new("Cat", "Dog").rename
     end
 
-    it 'renames the file' do
-      expect(File.exists?('./tmp/dog.rb')).to be true
+    it 'renames the files' do
+      expect(File.exists?('./app/models/dog.rb')).to be true
+      expect(File.exists?('./app/controllers/dogs_controller.rb')).to be true
     end
 
     it 'changes Cat to Dog' do
-      expect(File.read('./tmp/dog.rb')).to eq(dog_content)
+      expect(File.read('./app/models/dog.rb')).to eq(dog_class_content)
+      expect(File.read('./app/controllers/dogs_controller.rb')).to eq(dog_controller_content)
     end
   end
 end
